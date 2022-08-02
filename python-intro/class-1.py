77bbffa7e245da4944b7eaa63f7f8bdca4ea5d97
Gabriele Cardoso das Virgens <gabrielecardosov@gmail.com>
2022-08-02 17:12:40 -0300

    Variables and arithmetic operators

---
+++ python-intro/class-1.py
@@ -0,0 +1,16 @@
+print('Hello World!!\n')
+
+a = int(input('Type the first value: '))
+b = int(input('Type the second value: '))
+
+sum = a + b
+subtraction = a - b
+multiplication = a * b
+division = a / b
+remainder = a % b
+
+print('\nsum: ' + str(sum))
+print('subtraction: ' + str(subtraction))
+print('multiplication: ' + str(multiplication))
+print('division: {}'.format(division))
+print('remainder: {}'.format(remainder))
