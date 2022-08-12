1a0e999743d036d4a837cbb5ef5637fbae135b39
Gabriele Cardoso das Virgens <gabrielecardosov@gmail.com>
2022-08-12 15:34:05 -0300

    OOP: Classes, methods and functions

---
+++ python-intro/class-6.py
@@ -0,0 +1,23 @@
+class Calculator:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+    def sum(self):
+        return self.a + self.b
+
+    def sub(self):
+        return self.a - self.b
+
+    def mult(self):
+        return self.a * self.b
+
+    def div(self):
+        return self.a / self.b
+
+
+calculator = Calculator(10, 2)
+print('Sum: {}'.format(calculator.sum()))
+print('Subtraction: {}'.format(calculator.sub()))
+print('Multiplication: {}'.format(calculator.mult()))
+print('Division: {}'.format(calculator.div()))
